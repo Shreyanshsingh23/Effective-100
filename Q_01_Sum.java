912a125928d9b5b74462892b2c77403efc4af5d6
Shreyanshsingh23 <shreyanshrewa@gmail.com>
2023-12-02 23:27:52 +0530

    Initial commit

---
+++ Q_01_Sum.java
@@ -0,0 +1,7 @@
+class Q_01_Sum{
+    public static void main(String[] args){
+        int a = 33;
+        int b = 47;
+        System.out.println(a+b);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Effective-100
+ Here I am going to push solutions of practice sheet (Effective 100)
