587aac2114d36849c2f38da8491c01d752182379
Shreyanshsingh23 <shreyanshrewa@gmail.com>
2023-12-12 22:35:31 +0530

    done

--- print_three_digit_armstong_numbers.java
+++ print_three_digit_armstong_numbers.java
@@ -1,14 +1,15 @@
 public class print_three_digit_armstong_numbers {
     public static void main(String[] args){
 
+        System.out.println("Three digit armstrong numbers are :- ");
         armstrong_numbers();
     }
 
     static void armstrong_numbers() {
      
-        int n = 100;
+        int n = 100;   // because smalllest three digit number is 100
          
-        while(n <  1000){
+        while(n <  1000){   // and largest three digit number is 999
            int temp = n;
             int sum = 0;
 
@@ -24,6 +25,7 @@ public class print_three_digit_armstong_numbers {
             if(sum==temp){
                 System.out.println(temp);
             }
+            n = temp;
             n++;
         }
 
